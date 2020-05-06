d642b1d2f0f558baecd6d5ab3bb77ef76f89ef4b
Build System <sales@hollywoodcamerawork.com>
2020-05-06 17:11:50 +0200

    Random change

--- hello/main.go
+++ hello/main.go
@@ -20,7 +20,7 @@ func Handler(ctx context.Context) (Response, error) {
 	var buf bytes.Buffer
 
 	body, err := json.Marshal(map[string]interface{}{
-		"message": "hello1",
+		"message": "hello1 hhhhhhhhhhhhhhhhhhhh",
 	})
 	if err != nil {
 		return Response{StatusCode: 404}, err
--- world/main.go
+++ world/main.go
@@ -20,7 +20,7 @@ func Handler(ctx context.Context) (Response, error) {
 	var buf bytes.Buffer
 
 	body, err := json.Marshal(map[string]interface{}{
-		"message": "World1",
+		"message": "World1 wwwwwwwwwww",
 	})
 	if err != nil {
 		return Response{StatusCode: 404}, err
